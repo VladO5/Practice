12021103018945e8d87869c3858e4a040f8e53a1
VladO5 <114669797+VladO5@users.noreply.github.com>
2023-07-07 14:01:52 +0300

    ConsoleInput Module.rb

---
+++ Task2/ConsoleInput Module.rb
@@ -0,0 +1,58 @@
+require_relative "../Task1/Validation Module.rb"
+
+module ConsoleInput
+  include Validation
+
+  def get_string(prompt)
+    loop do
+      print prompt
+      string = gets.chomp
+      return string if string.is_a?(String) && !string.empty?
+
+      puts "Invalid input. Please try again."
+    end
+  end
+
+  def get_name(prompt)
+    loop do
+      print prompt
+      name= gets.chomp
+      return name if Validation.validate_name?(name)
+
+      puts "Invalid Name. Please try again"
+    end
+  end
+
+  def get_surname(prompt)
+    loop do
+      print prompt
+      surname= gets.chomp
+      return surname if Validation.validate_surname?(surname)
+
+      puts "Invalid surname. Please try again"
+    end
+  end
+
+  def get_inn(prompt)
+    loop do
+      print prompt
+      inn= gets.chomp
+      return inn if Validation.validate_inn?(inn)
+
+      puts "Invalid INN. Please try again"
+    end
+  end
+
+  def get_date(prompt)
+    require 'date'
+
+    loop do
+      print prompt
+      date_str = gets.chomp
+        date = Date.parse(date_str)
+        return date if Validation.validate_date?(date)
+
+        puts "Invalid date. Please enter a date earlier than today."
+    end
+  end
+end
